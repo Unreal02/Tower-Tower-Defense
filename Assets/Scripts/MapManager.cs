545949de53bd438f76da2afee5e2a47d5885ff4e
Unreal02 <scott0202@naver.com>
2022-01-15 15:11:00 +0900

    블럭 머터리얼 설정을 코드가 아닌 에디터에서 하도록 설정

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -6,13 +6,13 @@ public class MapManager : MonoBehaviour
 {
 	public Vector3 size;
 	public GameObject block;
+	public Material material1; // 테마 1
+	public Material material2; // 테마 2
+	public Material material3; // 입구 및 출구
 
 	private Vector3[] path;
 	private Vector3 pathStart;
 	private Vector3 pathEnd;
-	private GameObject bottom;
-	private GameObject wallXPlus;
-	private GameObject wallZPlus;
 
 	void Start()
 	{
@@ -23,40 +23,35 @@ public class MapManager : MonoBehaviour
 		pathStart = path[0];
 		pathEnd = path[path.Length - 1];
 
-		bottom = new GameObject();
-		wallXPlus = new GameObject();
-		wallZPlus = new GameObject();
-
-		bottom.transform.parent = transform;
-		wallXPlus.transform.parent = transform;
-		wallZPlus.transform.parent = transform;
-
 		GameObject temp;
 		for (int x = 0; x < size.x; x++)
 			for (int z = 0; z < size.z; z++)
 			{
-				temp = Instantiate(block, new Vector3(x, -1, z), Quaternion.identity, bottom.transform);
-				if ((x + z) % 2 == 0) temp.GetComponent<MeshRenderer>().material.color = Color.gray;
+				temp = Instantiate(block, new Vector3(x, -1, z), Quaternion.identity, transform);
+				if ((x + z) % 2 == 0) temp.GetComponent<MeshRenderer>().material = material1;
+				else temp.GetComponent<MeshRenderer>().material = material2;
 			}
 
-		for (int x = (int)pathStart.x - 1; x <= pathStart.x + 1; x++)
-			for (int y = (int)pathStart.y - 1; y <= pathStart.y + 1; y++)
-				for (int z = (int)pathStart.z - 1; z <= pathStart.z + 1; z++)
+		for (int x = (int)pathStart.x - 1; x <= (int)pathStart.x + 1; x++)
+			for (int y = (int)pathStart.y - 1; y <= (int)pathStart.y + 1; y++)
+				for (int z = (int)pathStart.z - 1; z <= (int)pathStart.z + 1; z++)
 				{
 					if (pathStart.x >= size.x && x != (int)pathStart.x) continue;
 					if (pathStart.z >= size.z && z != (int)pathStart.z) continue;
-					temp = Instantiate(block, new Vector3(x, y, z), Quaternion.identity, wallXPlus.transform);
-					if (new Vector3(x, y, z) != pathStart) temp.GetComponent<MeshRenderer>().material.color = Color.black;
+					temp = Instantiate(block, new Vector3(x, y, z), Quaternion.identity, transform);
+					if (new Vector3(x, y, z) == pathStart) temp.GetComponent<MeshRenderer>().material = material1;
+					else temp.GetComponent<MeshRenderer>().material = material3;
 				}
 
-		for (int x = (int)pathEnd.x - 1; x <= pathEnd.x + 1; x++)
-			for (int y = (int)pathEnd.y - 1; y <= pathEnd.y + 1; y++)
-				for (int z = (int)pathEnd.z - 1; z <= pathEnd.z + 1; z++)
+		for (int x = (int)pathEnd.x - 1; x <= (int)pathEnd.x + 1; x++)
+			for (int y = (int)pathEnd.y - 1; y <= (int)pathEnd.y + 1; y++)
+				for (int z = (int)pathEnd.z - 1; z <= (int)pathEnd.z + 1; z++)
 				{
 					if (pathEnd.x >= size.x && x != (int)pathEnd.x) continue;
 					if (pathEnd.z >= size.z && z != (int)pathEnd.z) continue;
-					temp = Instantiate(block, new Vector3(x, y, z), Quaternion.identity, wallXPlus.transform);
-					if (new Vector3(x, y, z) != pathEnd) temp.GetComponent<MeshRenderer>().material.color = Color.black;
+					temp = Instantiate(block, new Vector3(x, y, z), Quaternion.identity, transform);
+					if (new Vector3(x, y, z) == pathEnd) temp.GetComponent<MeshRenderer>().material = material1;
+					else temp.GetComponent<MeshRenderer>().material = material3;
 				}
 	}
 
