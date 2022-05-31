318d9a0cf4b3d9366bccf69d60b3c5e41c5833dd
Unreal02 <scott0202@kaist.ac.kr>
2022-06-01 03:46:49 +0900

    RoundInfoConverter.py 에 print 추가

--- Assets/Resources/RoundInfoConverter.py
+++ Assets/Resources/RoundInfoConverter.py
@@ -16,6 +16,8 @@ for r in range(2, ws.max_row + 1):
         cur_round = round
         raw_data.append([])
     raw_data[cur_round].append((time, enemyID))
+print(raw_data)
+print()
 
 data = {'rounds': []}
 for (round, spawn_list) in enumerate(raw_data):
@@ -25,6 +27,7 @@ for (round, spawn_list) in enumerate(raw_data):
     for spawn in spawn_list:
         spawns['spawns'].append({'time': spawn[0], 'enemy': "Enemy" + str(spawn[1])})
     data['rounds'].append(spawns)
+print(data)
 
 with open('./RoundInfo.json', 'w') as file:
     json.dump(data, file, indent = 4)
